50625f69dbc567e1fdedb99c643a2b4cff2e6cb5
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-23 10:29:11 -0300

    Como funciona as interfaces

--- bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/introducao-typescript/dio-typescript/index.ts
+++ bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/introducao-typescript/dio-typescript/index.ts
@@ -3,3 +3,30 @@ function soma(a: number, b: number) {
 }
 
 console.log(soma(3, 7));
+
+//Types
+//Interfaces
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean;
+}
+
+interface IAnimal {
+  nome: string;
+  tipo: "terrestre" | "aquático";
+  executarSom(som: number): void;
+}
+
+const animal: IAnimal = {
+  nome: "Elefante",
+  tipo: "terrestre",
+  executarSom: (som)=>(`${som}`)
+};
+
+animal.executarSom(5);
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    executarSom: (som)=>(`${som}`)
+}
