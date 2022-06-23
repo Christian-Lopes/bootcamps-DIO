6b93437245584025043e3412c8bc5fe5faaf15e1
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-23 10:44:58 -0300

    Como funcionam os types

--- bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/introducao-typescript/dio-typescript/index.ts
+++ bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/introducao-typescript/dio-typescript/index.ts
@@ -4,29 +4,49 @@ function soma(a: number, b: number) {
 
 console.log(soma(3, 7));
 
-//Types
 //Interfaces
-interface IFelino extends IAnimal{
-    visaoNoturna: boolean;
+//Usado para definir contratos
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
 }
 
 interface IAnimal {
   nome: string;
   tipo: "terrestre" | "aquático";
   executarSom(som: number): void;
+  domestico: boolean;
+}
+
+interface ICanino extends IAnimal {
+  porte: "pequeno" | "médio" | "grande";
 }
 
 const animal: IAnimal = {
   nome: "Elefante",
   tipo: "terrestre",
-  executarSom: (som)=>(`${som}`)
+  executarSom: (som) => `${som}`,
+  domestico: false,
 };
 
 animal.executarSom(5);
 
 const felino: IFelino = {
-    nome: 'Leão',
-    tipo: 'terrestre',
-    visaoNoturna: true,
-    executarSom: (som)=>(`${som}`)
-}
+  nome: "Leão",
+  tipo: "terrestre",
+  visaoNoturna: true,
+  executarSom: (som) => `${som}`,
+  domestico: false,
+};
+
+//Types
+
+type IDomestico = IFelino | ICanino;
+
+const animal2: IDomestico = {
+  domestico: true,
+  nome: "cachorro",
+  porte: "médio",
+  tipo: "terrestre",
+  executarSom: (som) => `${som}`,
+  visaoNoturna: true
+};
