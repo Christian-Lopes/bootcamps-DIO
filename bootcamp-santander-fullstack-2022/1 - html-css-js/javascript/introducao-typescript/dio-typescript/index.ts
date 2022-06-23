e4fc7d3edd279c3b4f106f74def81c5768ea6d40
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-23 11:29:39 -0300

    Desenvolvendo condicionais a partir de parâmetros

--- bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/introducao-typescript/dio-typescript/index.ts
+++ bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/introducao-typescript/dio-typescript/index.ts
@@ -48,22 +48,42 @@ const animal2: IDomestico = {
   porte: "médio",
   tipo: "terrestre",
   executarSom: (som) => `${som}`,
-  visaoNoturna: true
+  visaoNoturna: true,
 };
 
 //Input
 
-const input = document.getElementById('input') as HTMLInputElement;
+const input = document.getElementById("input") as HTMLInputElement;
 
-input.addEventListener('input', (event) => {
-   const i =event.currentTarget as HTMLInputElement;
-   console.log(i.value);
-})
+input.addEventListener("input", (event) => {
+  const i = event.currentTarget as HTMLInputElement;
+  console.log(i.value);
+});
 
 //Generic types
 
-function adicionarLista<T>(array: any[], valor: T){
-    return array.map(item => item + valor);
+function adicionarLista<T>(array: any[], valor: T) {
+  return array.map((item) => item + valor);
 }
 
 adicionarLista([1, 2, 4], 9);
+adicionarLista(["a", "bc"], "a");
+
+//Desenvolvendo condicionais a partir de parâmetros
+
+interface IUsuario {
+  id: string;
+  email: string;
+}
+
+interface IAdmin extends IUsuario {
+  cargo: "gerente" | "coordenador" | "supervisor";
+}
+
+function redirecione(usuario: IUsuario | IAdmin) {
+  if ("cargo" in usuario) {
+    //redirecionar para área de admin
+  }
+
+  //redirecionar para área do user
+}
