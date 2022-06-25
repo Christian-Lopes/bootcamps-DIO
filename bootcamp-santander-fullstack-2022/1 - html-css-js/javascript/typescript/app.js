24cfeaa30fcc2ef901afe832128fff132e26be4d
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-25 13:22:48 -0300

    Variável do tipo any

--- bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/typescript/app.js
+++ bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/typescript/app.js
@@ -1,11 +1,59 @@
 "use strict";
-const nameInput = document.getElementById('name');
-const btnName = document.getElementById('btn-name');
-function printName(name) {
-    return console.log(name);
+const nameInput = document.getElementById("name");
+const btnName = document.getElementById("btn-name");
+function printName(name, frase, imprimir) {
+    if (imprimir) {
+        return console.log(frase + name);
+    }
+    return console.log("Não foi permitido!");
 }
+let frase = "Seu nome é: ";
+let imprimir = true;
 if (btnName) {
-    btnName.addEventListener('click', () => {
-        printName(nameInput.value);
+    btnName.addEventListener("click", () => {
+        printName(nameInput.value, frase, imprimir);
     });
 }
+//Ojetos, variáveis e enum
+const person = {
+    name: "Christian",
+    age: 33,
+    profissao: "developer",
+};
+var Profissao;
+(function (Profissao) {
+    Profissao[Profissao["teacher"] = 0] = "teacher";
+    Profissao[Profissao["developer"] = 1] = "developer";
+    Profissao[Profissao["doctor"] = 2] = "doctor";
+    Profissao[Profissao["artist"] = 3] = "artist";
+})(Profissao || (Profissao = {}));
+const person1 = {
+    name: 'Christian',
+    age: 33,
+    profissao: Profissao.developer
+};
+console.log(person1);
+const aluno1 = {
+    name: 'Gabriel',
+    age: 15,
+    profissao: Profissao.teacher,
+    materia: ['Protuguês', 'Matemática', 'Programação'],
+    nota: [9, 7, 6]
+};
+function list(list) {
+    for (const item of list) {
+        console.log('-', item);
+    }
+}
+console.log(aluno1);
+list(aluno1.materia);
+//Variável any
+let valorAny;
+valorAny = true;
+//String
+valorAny = 'Christian';
+let numero = 33;
+console.log('Teste de varáveis: ', numero);
+//Número definido como number 
+numero = valorAny;
+console.log('Variável definido como number: ', numero);
--- bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/typescript/app.ts
+++ bootcamp-santander-fullstack-2022/1 - html-css-js/javascript/typescript/app.ts
@@ -1,13 +1,89 @@
+const nameInput = document.getElementById("name") as HTMLInputElement;
+const btnName = document.getElementById("btn-name");
 
-const nameInput = document.getElementById('name') as HTMLInputElement;
-const btnName = document.getElementById('btn-name');
-
-function printName(name: string){
-    return console.log(name);
+function printName(name: string, frase: string, imprimir: boolean) {
+  if (imprimir) {
+    return console.log(frase + name);
+  }
+  return console.log("Não foi permitido!");
 }
 
-if(btnName){
-    btnName.addEventListener('click', () => {
-        printName(nameInput.value);
-    })
+let frase: string = "Seu nome é: ";
+let imprimir: boolean = true;
+
+if (btnName) {
+  btnName.addEventListener("click", () => {
+    printName(nameInput.value, frase, imprimir);
+  });
 }
+
+//Ojetos, variáveis e enum
+
+const person: {
+  name: string;
+  age: number;
+  profissao: string;
+} = {
+  name: "Christian",
+  age: 33,
+  profissao: "developer",
+};
+
+enum Profissao {
+  teacher,
+  developer,
+  doctor,
+  artist,
+}
+
+interface IPerson {
+  name: string;
+  age: number;
+  profissao: Profissao;
+}
+
+const person1: IPerson = {
+    name: 'Christian',
+    age: 33,
+    profissao: Profissao.developer
+}
+
+console.log(person1);
+
+interface IAluno extends IPerson{
+    materia: string[],
+    nota: number[]
+}
+
+const aluno1: IAluno = {
+    name: 'Gabriel',
+    age: 15,
+    profissao: Profissao.teacher,
+    materia: ['Protuguês', 'Matemática', 'Programação'],
+    nota: [9, 7, 6]
+}
+
+function list(list: string[]){
+    for(const item of list){
+        console.log('-', item )
+    }
+}
+
+console.log(aluno1);
+list(aluno1.materia);
+
+//Variável any
+
+let valorAny: any;
+valorAny = true;
+
+//String
+valorAny =  'Christian';
+
+let numero: number = 33;
+console.log('Teste de varáveis: ', numero);
+//Número definido como number 
+numero = valorAny;
+
+console.log('Variável definido como number: ', numero);
+
