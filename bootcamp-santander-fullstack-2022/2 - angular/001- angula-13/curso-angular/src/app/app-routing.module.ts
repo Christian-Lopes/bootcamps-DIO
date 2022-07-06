f778876ce8c2a8ad93143eb4279d717fdf4b89a0
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-07-06 18:28:50 -0300

    Tópico 17: Trabalhando com Router em Angular.

--- bootcamp-santander-fullstack-2022/2 - angular/001- angula-13/curso-angular/src/app/app-routing.module.ts
+++ bootcamp-santander-fullstack-2022/2 - angular/001- angula-13/curso-angular/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivesComponent } from './components/directives/directives.component';
 
-const routes: Routes = [];
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'parent', component: ParentDataComponent },
+  { path: 'directive', component: DirectivesComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'list', component: ListRenderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
