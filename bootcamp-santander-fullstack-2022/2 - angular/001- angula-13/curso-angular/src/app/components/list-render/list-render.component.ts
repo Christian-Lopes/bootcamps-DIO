a543aa56cd6aa843145f9f2b6fcdaa0923585298
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-07-06 11:26:14 -0300

    Tópico 13: Interfaces no angular

--- bootcamp-santander-fullstack-2022/2 - angular/001- angula-13/curso-angular/src/app/components/list-render/list-render.component.ts
+++ bootcamp-santander-fullstack-2022/2 - angular/001- angula-13/curso-angular/src/app/components/list-render/list-render.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Amorinha', type: 'dog' },
-    { name: 'Garfield', type: 'cat' },
-    { name: 'Rosa', type: 'bird' },
-    { name: 'Doginho', type: 'dog' },
-    { name: 'Tom', type: 'cat' },
-    { name: 'Jerry', type: 'mouse' },
+  animals: Animal[] = [
+    { name: 'Amorinha', type: 'dog', age: 4 },
+    { name: 'Garfield', type: 'cat', age: 5 },
+    { name: 'Rosa', type: 'bird', age: 7 },
+    { name: 'Doginho', type: 'dog', age: 5 },
+    { name: 'Tom', type: 'cat', age: 9 },
+    { name: 'Jerry', type: 'mouse', age: 9 },
   ];
-  
+
+  animalDetails = '';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} de idade.`;
+  }
 }
